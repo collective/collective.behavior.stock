29a8406cd1d6260b783f15b18c25fb9b608f8a85
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-09-17 23:15:42 +0300

    Documentation updated.

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'Plone>=4.2',
         'collective.cart.stock',
         'five.grok',
         'hexagonit.testing',
