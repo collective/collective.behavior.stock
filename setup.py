cef23396ec22817f5d555c1de75f1cdce7c1686c
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-10-28 11:13:54 +0200

    release 0.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='collective.behavior.stock',
-    version='0.5.0.3',
+    version='0.6',
     description="Provides stock related behavior to dexterity content types.",
     long_description=long_description,
     classifiers=[
